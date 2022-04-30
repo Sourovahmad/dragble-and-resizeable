e86fb9fc335a461e310883be6fc390b713d9dfac
sourovahmad <mdsourovahmad19@gmail.com>
2022-04-30 14:20:10 +0600

    adding element

--- script.js
+++ script.js
@@ -1,293 +1,62 @@
 
-var box = elementFinder("box");
-var boxWrapper = elementFinder("box-wrapper");
 
-const containerWidth = elementFinder('full_container').offsetWidth;
-const containerHeight = elementFinder('full_container').offsetHeight;
-
-var initX, initY, mousePressX, mousePressY, initW, initH, initRotate;
-
-const itemdimensions = [];
-itemdimensions[0] = {};
-itemdimensions[1] = {};
-
-
-const minWidth = 40;
-const minHeight = 40;
-
-
-
-
-
-
-function repositionElement(x, y) {
-
-    const LeftinPercentage = ((x*100)/containerWidth).toFixed(2);
-    const TopinPercentage = ((y*100)/containerHeight).toFixed(2);
-
-    boxWrapper.style.left = LeftinPercentage + '%';
-    boxWrapper.style.top = TopinPercentage + '%';
-
-    elementFinder('selected_item').innerHTML = 'left: ' + LeftinPercentage + "%" + ' top: ' + TopinPercentage + "%";
-
-    const item1 = {
-        'left': x,
-        'top': y
-    }
-    
-    itemdimensions[0] = item1;
-
-}
-
-
-
-// drag support
-boxWrapper.addEventListener('mousedown', function (event) {
-    if (event.target.className.indexOf("dot") > -1) {
-        return;
-    }
-
-    initX = this.offsetLeft;
-    initY = this.offsetTop;
-    mousePressX = event.clientX;
-    mousePressY = event.clientY;
-
-
-    function eventMoveHandler(event) {
-        repositionElement(initX + (event.clientX - mousePressX),
-            initY + (event.clientY - mousePressY));
-    }
-
-    boxWrapper.addEventListener('mousemove', eventMoveHandler, false);
-    window.addEventListener('mouseup', function eventEndHandler() {
-        boxWrapper.removeEventListener('mousemove', eventMoveHandler, false);
-        window.removeEventListener('mouseup', eventEndHandler);
-    }, false);
-
-}, false);
-// done drag support
-
-
-
-// resize section 
-
-function resize(w, h) {
-    box.style.width = w + 'px';
-    box.style.height = h + 'px';
-
-    elementFinder('box_1_dimensin_details').innerHTML =  w + "px" + "/" + h + "px";
-    console.log(w);
-}
-
-
-
-
-
-
-
+// Helpers funtions //
 
 function elementFinder(id){
     return document.getElementById(id);
 }
 
-
-
-var rightMid = document.getElementById("right-mid");
-var leftMid = document.getElementById("left-mid");
-var topMid = document.getElementById("top-mid");
-var bottomMid = document.getElementById("bottom-mid");
-
-var leftTop = document.getElementById("left-top");
-var rightTop = document.getElementById("right-top");
-var rightBottom = document.getElementById("right-bottom");
-var leftBottom = document.getElementById("left-bottom");
+// End of helper functions // 
 
 
 
-function resizeHandler(event, left = false, top = false, xResize = false, yResize = false) {
 
-    initX = boxWrapper.offsetLeft;
-    initY = boxWrapper.offsetTop;
-    mousePressX = event.clientX;
-    mousePressY = event.clientY;
 
-    initW = box.offsetWidth;
-    initH = box.offsetHeight;
+////// Add new Section //////
 
-    initRotate = getCurrentRotation(boxWrapper);
-    var initRadians = initRotate * Math.PI / 180;
-    var cosFraction = Math.cos(initRadians);
-    var sinFraction = Math.sin(initRadians);
+let currentSections = 2;
+
+elementFinder('addNewSectionButton').addEventListener('click', function(){
+
+    const newSectionNumber = currentSections += 1;
     
-    function eventMoveHandler(event) {
-        var wDiff = (event.clientX - mousePressX);
-        var hDiff = (event.clientY - mousePressY);
-        var rotatedWDiff = cosFraction * wDiff + sinFraction * hDiff;
-        var rotatedHDiff = cosFraction * hDiff - sinFraction * wDiff;
+    const newHtml = `<div id="full_Box_${newSectionNumber}">
+    <div class="box-wrapper" id="box-wrapper_${newSectionNumber}">
+        <div class="box_${newSectionNumber} box_section" id="box_${newSectionNumber}" data-current-box-number="${newSectionNumber}"> 
 
-        var newW = initW, newH = initH, newX = initX, newY = initY;
-
-        if (xResize) {
-            if (left) {
-                newW = initW - rotatedWDiff;
-                if (newW < minWidth) {
-                  newW = minWidth;
-                  rotatedWDiff = initW - minWidth;
-                }
-            } else {
-                newW = initW + rotatedWDiff;
-                if (newW < minWidth) {
-                  newW = minWidth;
-                  rotatedWDiff = minWidth - initW;
-                }
-            }
-            newX += 0.5 * rotatedWDiff * cosFraction;
-            newY += 0.5 * rotatedWDiff * sinFraction;
-        }
-
-        if (yResize) {
-            if (top) {
-                newH = initH - rotatedHDiff;
-                if (newH < minHeight) {
-                  newH = minHeight;
-                  rotatedHDiff = initH - minHeight;
-                }
-            } else {
-                newH = initH + rotatedHDiff;
-                if (newH < minHeight) {
-                  newH = minHeight;
-                  rotatedHDiff = minHeight - initH;
-                }
-            }
-            newX -= 0.5 * rotatedHDiff * sinFraction;
-            newY += 0.5 * rotatedHDiff * cosFraction;
-        }
-
-        resize(newW, newH);
-        repositionElement(newX, newY);
-    }
+            <div class="dot left-top" id="left-top_${newSectionNumber}"></div>
+            <div class="dot left-bottom" id="left-bottom_${newSectionNumber}"></div>
+            <div class="dot top-mid" id="top-mid_${newSectionNumber}"></div>
+            <div class="dot bottom-mid" id="bottom-mid_${newSectionNumber}"></div>
+            <div class="dot left-mid" id="left-mid_${newSectionNumber}"></div>
+            <div class="dot right-mid" id="right-mid_${newSectionNumber}"></div>
+            <div class="dot right-bottom" id="right-bottom_${newSectionNumber}"></div>
+            <div class="dot right-top" id="right-top_${newSectionNumber}"></div>
+            <p class="w-100 h-100 text-center d-flex justify-content-center align-items-center text-light flex-column"> <span> ${newSectionNumber} </span>  
+             </p>
+        </div>
+    </div>
+</div>`
 
 
-    window.addEventListener('mousemove', eventMoveHandler, false);
-    window.addEventListener('mouseup', function eventEndHandler() {
-        window.removeEventListener('mousemove', eventMoveHandler, false);
-        window.removeEventListener('mouseup', eventEndHandler);
-    }, false);
-}
+    elementFinder('full_container').insertAdjacentHTML('beforeend', newHtml);
+    var newWrapper = document.getElementById(`box-wrapper_${newSectionNumber}`);
+    
+    newWrapper.style.left = 50 + '%';
+    newWrapper.style.top = 50 + '%';
 
-
-
-function getCurrentRotation(el) {
-    var st = window.getComputedStyle(el, null);
-    var tm = st.getPropertyValue("-webkit-transform") ||
-        st.getPropertyValue("-moz-transform") ||
-        st.getPropertyValue("-ms-transform") ||
-        st.getPropertyValue("-o-transform") ||
-        st.getPropertyValue("transform")
-    "none";
-    if (tm != "none") {
-        var values = tm.split('(')[1].split(')')[0].split(',');
-        var angle = Math.round(Math.atan2(values[1], values[0]) * (180 / Math.PI));
-        return (angle < 0 ? angle + 360 : angle);
-    }
-    return 0;
-}
-
-
-
-
-rightMid.addEventListener('mousedown', e => resizeHandler(e, false, false, true, false));
-leftMid.addEventListener('mousedown', e => resizeHandler(e, true, false, true, false));
-topMid.addEventListener('mousedown', e => resizeHandler(e, false, true, false, true));
-bottomMid.addEventListener('mousedown', e => resizeHandler(e, false, false, false, true));
-leftTop.addEventListener('mousedown', e => resizeHandler(e, true, true, true, true));
-rightTop.addEventListener('mousedown', e => resizeHandler(e, false, true, true, true));
-rightBottom.addEventListener('mousedown', e => resizeHandler(e, false, false, true, true));
-leftBottom.addEventListener('mousedown', e => resizeHandler(e, true, false, true, true));
-
-
-
-
-
-repositionElement(200, 200);
-resize(80, 80);
-
-
-
-
-
-
-// box 2 
-
-var box_2 = document.getElementById("box_2");
-var box_2_wrapper = document.getElementById("box_2_wrapper");
-
-
-
-var initX_box_2, initY_box_2, mousePressX_box_2, mousePressY_box_2;
-
-function repositionElement_box_2(x, y) {
-
-    const LeftinPercentage_2 = ((x*100)/containerWidth).toFixed(2);
-    const TopinPercentage_2 = ((y*100)/containerHeight).toFixed(2);
-
-    box_2_wrapper.style.left = LeftinPercentage_2 + '%';
-    box_2_wrapper.style.top = TopinPercentage_2 + '%';
-    elementFinder('selected_item_2').innerHTML = 'left: ' + LeftinPercentage_2 + "%" + ' top: ' + TopinPercentage_2 + "%";
-
-    const item2 = {
-        'left': LeftinPercentage_2,
-        'top': TopinPercentage_2
-    }
-    itemdimensions[1] = item2;
-
-}
-
-
-
-// drag support
-box_2_wrapper.addEventListener('mousedown', function (event) {
-    if (event.target.className.indexOf("dot") > -1) {
-        return;
-    }
-
-    initX_box_2 = this.offsetLeft;
-    initY_box_2 = this.offsetTop;
-    mousePressX_box_2 = event.clientX;
-    mousePressY_box_2 = event.clientY;
-
-
-
-    // function eventMoveHandler(event) {
-    //     repositionElement(initX + (event.clientX - mousePressX),
-    //         initY + (event.clientY - mousePressY));
-    // }
-
-
-
-    function eventMoveHandler_2(event) {
-        repositionElement_box_2(initX_box_2 + (event.clientX - mousePressX_box_2),
-        initY_box_2 + (event.clientY - mousePressY_box_2));
-    }
-
-    box_2_wrapper.addEventListener('mousemove', eventMoveHandler_2, false);
-    window.addEventListener('mouseup', function eventEndHandler() {
-        box_2_wrapper.removeEventListener('mousemove', eventMoveHandler_2, false);
-        window.removeEventListener('mouseup', eventEndHandler);
-    }, false);
-
-}, false);
-// done drag support
-
-
-
-
-repositionElement_box_2(400, 200);
-
-
-
-elementFinder('savebutton').addEventListener('click', function(){
-    console.log(JSON.stringify(itemdimensions));
+  
 });
 
 
+const boxSection = document.getElementsByClassName('box_section');
+
+Array.prototype.forEach.call(boxSection, function(el) {
+    el.addEventListener('mousedown', function(){
+        const currentBoxNumber = this.getAttribute('data-current-box-number');
+        
+
+
+    });
+
+});
